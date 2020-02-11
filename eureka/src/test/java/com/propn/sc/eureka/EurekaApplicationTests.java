33fe28ec32f8eaff46858d25d2321a5b3e0ff900
propn <xulei33@msn.com>
2020-02-11 20:14:10 +0800

    Initial commit

---
+++ eureka/src/test/java/com/propn/sc/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.propn.sc.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
