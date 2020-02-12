1e276f7a2744b03eb0de7bd732becc489f81b3f0
propn <xulei33@msn.com>
2020-02-12 20:13:26 +0800

    eureka

--- producer/src/main/java/com/propn/sc/producer/HelloController.java
+++ producer/src/main/java/com/propn/sc/producer/HelloController.java
@@ -1,5 +1,7 @@
 package com.propn.sc.producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +13,21 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 public class HelloController {
 
+    public final static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @RequestMapping("/hello")
     public String index(@RequestParam String name) {
         //http://192.168.206.1:8080/hello?name=lei
+
+        logger.debug(name);
+        logger.trace("trace level");
+        logger.debug("debug level");
+        logger.info("info level");
+        logger.warn("warn level");
+        logger.error("error level");
+
         HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest();
-        return "hello "+name+"，this messge is from host"+request.getServerName() +":"+request.getServerPort();
+        return "hello " + name + "，this messge is from host" + request.getServerName() + ":" + request.getServerPort();
     }
 
 }
